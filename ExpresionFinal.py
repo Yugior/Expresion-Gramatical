b1549fa9148d294e7f9c97a9088da7f0c3d4fa68
Yugior <150100500+Yugior@users.noreply.github.com>
2025-04-28 23:40:48 -0600

    Add files via upload

---
+++ ExpresionFinal.py
@@ -0,0 +1,57 @@
+#Horacio Villela Hernandez A01712206 28/04/2025
+
+import nltk
+from nltk import CFG
+nltk.download('punkt')
+
+# Define a context-free grammar
+grammar = CFG.fromstring("""
+
+S -> NP VP
+
+VP -> VSimple
+VP -> VCompuesto
+VP -> VConPP
+
+VSimple -> V
+VCompuesto -> V NP
+VConPP -> V NP PP
+VConPP -> V PP
+
+NP -> Pron
+NP -> N
+NP -> Det N
+NP -> Det N PP
+NP -> P PossN
+
+PP -> P NP
+
+
+
+Det -> 'vaba' | 'ju' | 'vai' | 'voe' | 'vehvi' | 'vadu' | 'geruta' | 'devado' | 'sotvad'
+
+N -> 'vaba' | 'ju' | 'vai' | 'voe' | 'vehvi' | 'vadu' | 'geruta' | 'devado' | 'sotvad'
+
+Pron -> 'ani' | 'eso' | 'yaafu' | 'yaava'
+
+PossN -> 'vabani' | 'juso' | 'vadufu'
+
+V -> 'sareqso' | 'sosorq' | 'vurqso' | 'daraqso' | 'katvso' | 'sotreqso'
+
+P -> 'ager' | 'no' | 'vaq' | 'sha' | 'mahno' | 'solno'
+
+""")
+
+# Create a parser with the defined grammar
+parser = nltk.ChartParser(grammar)
+
+# Input sentence to be parsed
+sentence = "ju sosorq mahno vabani"
+
+# Tokenize the sentence
+tokens = sentence.split()
+
+
+# Parse the sentence
+for tree in parser.parse(tokens):
+    tree.pretty_print()
